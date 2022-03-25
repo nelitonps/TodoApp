58e0f50345f7b837f42e5c61629bc90806962bd8
nelitonps <neliton.net@hotmail.com>
2022-03-25 12:40:57 -0300

    Criação da classe de conexão com banco de dados.

--- app/src/main/java/TodoApp/App.java
+++ app/src/main/java/TodoApp/App.java
@@ -1,9 +1,14 @@
 
 package TodoApp;
 
+import java.sql.Connection;
+import util.ConnectionDB;
+
 public class App {
 
     public static void main(String[] args) {
-        
+            Connection c = ConnectionDB.getConnection();
+            
+            ConnectionDB.closeConnection(c);
     }
 }
