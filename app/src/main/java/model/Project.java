284558670d5161ab59db39bf8c5520132444ccdb
nelitonps <neliton.net@hotmail.com>
2022-03-25 10:51:15 -0300

    Criação da classe que representa o projeto.

--- app/src/main/java/model/Project.java
+++ app/src/main/java/model/Project.java
@@ -1,13 +1,65 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package model;
 
-/**
- *
- * @author NelitonPS
- */
+import java.util.Date;
+
 public class Project {
-    
+        private int id;
+        private String name;
+        private String description;
+        private Date createDat;
+        private Date updateDat;
+
+        public Project(int id, String name, String description, Date createDat, Date updateDat) {
+                this.id = id;
+                this.name = name;
+                this.description = description;
+                this.createDat = createDat;
+                this.updateDat = updateDat;
+        }
+
+        public int getId() {
+                return id;
+        }
+
+        public void setId(int id) {
+                this.id = id;
+        }
+
+        public String getName() {
+                return name;
+        }
+
+        public void setName(String name) {
+                this.name = name;
+        }
+
+        public String getDescription() {
+                return description;
+        }
+
+        public void setDescription(String description) {
+                this.description = description;
+        }
+
+        public Date getCreateDat() {
+                return createDat;
+        }
+
+        public void setCreateDat(Date createDat) {
+                this.createDat = createDat;
+        }
+
+        public Date getUpdateDat() {
+                return updateDat;
+        }
+
+        public void setUpdateDat(Date updateDat) {
+                this.updateDat = updateDat;
+        }
+
+    @Override
+    public String toString() {
+        return "Project{" + "id=" + id + ", name=" + name + ", description=" + description + ", createDat=" + createDat + ", updateDat=" + updateDat + '}';
+    }
 }
